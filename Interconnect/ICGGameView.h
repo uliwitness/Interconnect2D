b4e4902cf3b2f4efe0dd427874a1acbb1e758e7a
Uli Kusterer <ulivc@zathras.de>
2015-08-21 21:58:45 +0200

    Add application icon.

---
+++ Interconnect/ICGGameView.h
@@ -0,0 +1,13 @@
+//
+//  ICGGameView.h
+//  Interconnect
+//
+//  Created by Uli Kusterer on 21/08/15.
+//  Copyright (c) 2015 Uli Kusterer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ICGGameView : NSView
+
+@end
---
+++ Interconnect/ICGGameView.m
@@ -0,0 +1,19 @@
+//
+//  ICGGameView.m
+//  Interconnect
+//
+//  Created by Uli Kusterer on 21/08/15.
+//  Copyright (c) 2015 Uli Kusterer. All rights reserved.
+//
+
+#import "ICGGameView.h"
+
+@implementation ICGGameView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
