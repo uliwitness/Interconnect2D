36eb0ff6c1f8ca88c74c8002f63a0ae7b85b3541
Uli Kusterer <ulivc@zathras.de>
2015-09-12 20:53:23 +0200

    Some debug code for tracking down why ICGConversationChoice gets released when it shouldn't.

---
+++ Interconnect/ICGConversation+ICGConversationMemoryDebug.h
@@ -0,0 +1,13 @@
+//
+//  ICGConversation+ICGConversationMemoryDebug.h
+//  Interconnect
+//
+//  Created by Uli Kusterer on 11/09/15.
+//  Copyright (c) 2015 Uli Kusterer. All rights reserved.
+//
+
+#import "ICGConversation.h"
+
+@interface ICGConversation (ICGConversationMemoryDebug)
+
+@end
---
+++ Interconnect/ICGConversation+ICGConversationMemoryDebug.m
@@ -0,0 +1,31 @@
+//
+//  ICGConversation+ICGConversationMemoryDebug.m
+//  Interconnect
+//
+//  Created by Uli Kusterer on 11/09/15.
+//  Copyright (c) 2015 Uli Kusterer. All rights reserved.
+//
+
+#import "ICGConversation+ICGConversationMemoryDebug.h"
+
+@implementation ICGConversation (ICGConversationMemoryDebug)
+
+-(id) retain
+{
+    return [super retain];
+}
+
+
+-(void) release
+{
+    [super release];
+}
+
+
+-(id) autorelease
+{
+    return [super autorelease];
+}
+
+
+@end
